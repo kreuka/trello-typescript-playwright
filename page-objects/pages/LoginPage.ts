24994dedb64a42fd71fe19e83f078c1f6b09087d
kreuka <reuka.kirill@gmail.com>
2024-04-23 17:47:15 +0300

    updated locator

--- page-objects/pages/LoginPage.ts
+++ page-objects/pages/LoginPage.ts
@@ -4,7 +4,7 @@ import BasePage from "../BasePage";
 export default class LoginPage extends BasePage {
   pagePath = "/login";
   private loginForm = this.page.getByRole("main");
-  private emailInput = this.page.getByPlaceholder("Enter email");
+  private emailInput = this.page.getByPlaceholder("Enter your email");
   private passwordInput = this.page.getByPlaceholder("Enter password");
   private logInButton = this.page.getByRole("button", { name: "Log in" });
   private continueButton = this.page.getByRole("button", { name: "Continue" });
