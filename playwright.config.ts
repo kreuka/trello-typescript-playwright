c77d0bf829939e7eb236150668c70a25928815c3
kreuka <reuka.kirill@gmail.com>
2024-04-23 17:55:44 +0300

    updated github actions workflow

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,13 +13,13 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 4 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ["html"],
     ["monocart-reporter", {  
       name: `Monocart Test Report ${new Date().toISOString().slice(0, 10)}`,
-      outputFile: "./test-results/report.html"
+      outputFile: "./playwright-report/monocart-report.html"
     }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
