7a35ea9d0cdc3c9b6c17a787757c2a4ffc908be6
Kyrylo Reuka <81745152+kreuka@users.noreply.github.com>
2024-05-29 17:05:55 +0300

    added afterEach to remove created boards (#4)

--- api/controllers/board.controller.ts
+++ api/controllers/board.controller.ts
@@ -3,6 +3,7 @@ import { APIRoutes } from "../../utils/constants/routes";
 import { CreateBoard } from "../builders/entities/boardBuilder";
 import { CreateBoardResponse } from "../types/CreateBoardResponse";
 import { BaseController } from "./base.controller";
+import { GetBoardsMemberBelongsToResponse } from "../types/GetBoardsMemberBelongsToResponse";
 
 export class BoardController extends BaseController {
   async createBoard(data: CreateBoard): Promise<CreateBoardResponse> {
@@ -13,10 +14,28 @@ export class BoardController extends BaseController {
       .send() as Promise<CreateBoardResponse>;
   }
 
-  async deleteBoard(boardId: string): Promise<APIResponse> {
+  async deleteBoardById(boardId: string): Promise<APIResponse> {
     return await this.request()
       .setUrl(`${process.env.API_BASE_URL}${APIRoutes.Boards}/${boardId}`)
       .setMethod("DELETE")
       .send() as Promise<APIResponse>;
+  }
+
+  async deleteBoardByTitle(boardTitle: string): Promise<void> {
+    const boardId = await this.getBoardIdByTitle(boardTitle);
+    if (boardId) await this.deleteBoardById(boardId);
+  }
+
+  async getBoardsMemberBelongsTo(memberId: string = process.env.TRELLO_MEMBER_ID!): Promise<GetBoardsMemberBelongsToResponse[]> {  
+    return await this.request()
+      .setUrl(`${process.env.API_BASE_URL}${APIRoutes.Members}/${memberId}${APIRoutes.Boards}`)
+      .setMethod("GET")
+      .send() as Promise<GetBoardsMemberBelongsToResponse[]>;
+  }
+
+  async getBoardIdByTitle(boardTitle: string): Promise<string | undefined> {
+    const allBoards: GetBoardsMemberBelongsToResponse[] = await this.getBoardsMemberBelongsTo();
+    const board = allBoards.find((board) => board.name === boardTitle);
+    return board ? board.id : undefined;
   }
 }
--- tests/board.spec.ts
+++ tests/board.spec.ts
@@ -2,18 +2,29 @@ import { loggedUserFixture } from "../fixtures/loggedUserFixture";
 import { loggedUserWithBoardFixture } from "../fixtures/loggedUserWithBoardFixture";
 import DataGenerator from "./../helpers/DataGenerator";
 import BoardPage from "../page-objects/pages/BoardPage";
+import test from "@playwright/test";
+import { BoardController } from "../api/controllers/board.controller";
 
-loggedUserFixture("@TC-2, Verify the ability to create a board", async ({  page, homePage }) => {
-  // TODO delete a created board
-  await homePage.header.clickOnCreate();
-  await homePage.header.chooseCreateBoardOptionFromCreateDropdown();
+let boardTitle: string;
 
-  const boardTitle = DataGenerator.getPhrase();
-  await homePage.header.createMenu.fillBoardTitle(boardTitle);
-  await homePage.header.createMenu.clickOnCreateBoardButton();
-  
-  const boardPage = new BoardPage(page);
-  await boardPage.verifyBoardTitle(boardTitle);
+test.describe("Board Tests that requires board deletion not in a fixture", () => {
+  test.afterEach(async ({ request }) => {
+    if (boardTitle) {
+      await new BoardController(request).deleteBoardByTitle(boardTitle);
+    }
+  });
+
+  loggedUserFixture("@TC-2 Verify the ability to create a board", async ({ page, homePage }) => {
+    await homePage.header.clickOnCreate();
+    await homePage.header.chooseCreateBoardOptionFromCreateDropdown();
+
+    boardTitle = DataGenerator.getPhrase();
+    await homePage.header.createMenu.fillBoardTitle(boardTitle);
+    await homePage.header.createMenu.clickOnCreateBoardButton();
+    
+    const boardPage = new BoardPage(page);
+    await boardPage.verifyBoardTitle(boardTitle);
+  });
 });
 
 loggedUserWithBoardFixture("@TC-5, Verify the ability to delete a board throught the Workspace navigation panel", async ({ board, boardPage }) => {
